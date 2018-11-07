42ffab404099ddd36da878439ffa7c5be641d76c
ilaganf <ilaganf@stanford.edu>
2018-11-06 23:57:50 -0800

    added notes and stuff

--- analyze.py
+++ analyze.py
@@ -3,4 +3,19 @@ analyze.py
 
 Analyzes a given StyleGraph object, writes the analysis to file,
 and returns a style vector
+
+Things in the style vector:
+- the motif vector
+- avg clustering coefficient
+- avg graph width
+- rolX vector
+- avg degree
+- what else?
 '''
+import snap
+
+def gen_style_vec(graph):
+    '''
+    Given StyleGraph, return a style vector
+    '''
+    pass
