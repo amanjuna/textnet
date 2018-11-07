ade44a5af6a49d06ef943d812f04fe08e8a2ed6c
Anoop Manjunath <amanjuna@stanford.edu>
2018-11-06 21:49:52 -0800

    initial commit

---
+++ Graph_Generator.py
@@ -0,0 +1,15 @@
+"""
+Graph_Generator.py
+
+Template class specifying generic functions for Graph_generator
+Class
+
+"""
+
+import
+
+class Graph_Generator:
+    def __init__(self, txt_file):
+        self.txt_file = txt_file
+
+    def
