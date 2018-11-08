908cfd96bd81e57ab662d9b1b9ddcc8f2e2cbdee
Anoop Manjunath <amanjuna@stanford.edu>
2018-11-07 21:28:08 -0800

    whatever

--- GraphGenerator.py
+++ GraphGenerator.py
@@ -6,22 +6,13 @@ Class
 
 """
 
+from utils import *
 import numpy as np
 import os, pickle, nltk
 
-EMBEDDINGS = "./embeddings/glove.6B.300d.txt"
-EMBEDDING_DIMS = (400000, 300)
-
 class GraphGenerator:
-    def __init__(self, txt_file, load_emb=False):
-        emb_exists = os.path.isfile("./embeddings/emb.pickle")
-        if load_emb or not emb_exists:
-            print("Loading embeddings from scratch - will take a moment")
-            emb_data = self.load_embeddings()
-            pickle.dump(emb_data, open("./embeddings/emb.pickle", 'wb'))
-        emb_data = pickle.load(open("./embeddings/emb.pickle", 'rb'))
-        print("Embeddings loaded")
-        self.word2id_dict, self.id2word_dict, self.emb = emb_data
+    def __init__(self, txt_file, emb, word2id, id2word):
+        self.emb, self.word2id_id, self.id2word_id = emb, word2id, id2word
         self.tokens = self.tokenize(txt_file)
 
     def generate_graph(self):
@@ -44,19 +35,7 @@ class GraphGenerator:
     def id2word(self, id):
         return self.id2word_dict[id]
 
-    def load_embeddings(self):
-        word2id_dict, id2word_dict = {}, {}
-        embeddings = np.zeros(EMBEDDING_DIMS)
-        with open(EMBEDDINGS) as open_emb:
-            i = 0
-            for line in open_emb:
-                line = line.strip().split(' ')
-                word2id_dict[line[0]] = i
-                id2word_dict[i] = line[0]
-                embeddings[i,:] = list(map(float, line[1:]))
-                i += 1
-        return word2id_dict, id2word_dict, embeddings
-
 if __name__=="__main__":
+    emb, load_embeddings()
     gg = GraphGenerator("gita.txt")
     #gg.load_embeddings()
--- utils.py
+++ utils.py
@@ -3,15 +3,22 @@
 EMBEDDINGS = "./embeddings/glove.6B.300d.txt"
 EMBEDDING_DIMS = (400000, 300)
 
-def load_embeddings(self):
-    word2id_dict, id2word_dict = {}, {}
-    embeddings = np.zeros(EMBEDDING_DIMS)
-    with open(EMBEDDINGS) as open_emb:
-        i = 0
-        for line in open_emb:
-            line = line.strip().split(' ')
-            word2id_dict[line[0]] = i
-            id2word_dict[i] = line[0]
-            embeddings[i,:] = list(map(float, line[1:]))
-            i += 1
-    return word2id_dict, id2word_dict, embeddings
+def load_embeddings(reload_emb=False):
+    emb_exists = os.path.isfile("./embeddings/emb.pickle")
+    if reload_emb or not emb_exists:
+        print("Loading embeddings from scratch - will take a moment")
+        word2id_dict, id2word_dict = {}, {}
+        embeddings = np.zeros(EMBEDDING_DIMS)
+        with open(EMBEDDINGS) as open_emb:
+            i = 0
+            for line in open_emb:
+                line = line.strip().split(' ')
+                word2id_dict[line[0]] = i
+                id2word_dict[i] = line[0]
+                embeddings[i,:] = list(map(float, line[1:]))
+                i += 1
+            pickle.dump(open("./embeddings/emb.pickle", 'wb'), [embeddings, word2id_dict, id2word_dict])
+    else:
+        word2id_dict, id2word_dict, embeddings = pickle.load(open("./embeddings/emb.pickle", 'rb'))
+    print("Embeddings loaded")
+    return embeddings, word2id_dict, id2word_dict, embeddings
