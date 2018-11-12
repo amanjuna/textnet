eb6babdaf8d11342332aad48aedb8a67fc6486ae
Vik Arjun Pattabi <vpattabi@rice08.stanford.edu>
2018-11-12 00:04:27 -0800

    Debugging TextPOS?

--- TextPOS.py
+++ TextPOS.py
@@ -10,13 +10,17 @@ import scipy.spatial as ss
 class TextPOS(GraphGenerator):
     def generate_graph(self):
         tagged = nltk.pos_tag(self.tokens)
-        relationship = set(['VERB', 'CONJ', 'ADP']) #ADP is 'adposition'
-        node_names = set([word[0] for word in tagged if (word[1] != 'DET' and word[1] != '.' and word[1] not in relationship)])  #Discard determiners/connectors
-        G = nx.Graph()
-        for node in node_names: G.add_node(node)
+        relationship = set(['VB', 'VBD', 'VBN', 'VBP', 'VBZ', 'IN', 'CC']) #ADP is 'adposition'
+	determiner = set(['WDT', 'DT', 'PDT'])
+        names_list = [word[0] for word in tagged if (word[1] not in determiner and word[1] != '.' and word[1] not in relationship)]  #Discard determiners/connectors
+        node_names = set(names_list)
+	G = nx.Graph()
+        print(names_list)
+	print(tagged)
+	for node in node_names: G.add_node(node)
 
-        start_node = tagged[0]
-        start_sentence = False
+        start_node = None
+        start_sentence = True
         for word_i in tagged[1:]:
             if start_sentence:
                 if word_i[0] in node_names:
@@ -25,14 +29,13 @@ class TextPOS(GraphGenerator):
                     continue
                 else:
                     continue
-            if word_i[1] in relationship or word_i[1] == 'DET':
+            if word_i[1] in relationship or word_i[1] in determiner:
                 continue
             if word_i[1] == '.':
                 start_sentence = True
                 continue
             G.add_edge(start_node[0], word_i[0])
             start_node  = word_i
-        print(G)
         return G
 
 if __name__=="__main__":
