cac956b9d785b141835482b6ca121b783ca5174f
ilaganf <ilaganf@stanford.edu>
2018-12-08 15:24:38 -0800

    testing chaingraph

--- graph_generation/ChainGraph.py
+++ graph_generation/ChainGraph.py
@@ -3,13 +3,19 @@ Graph_Generator.py
 Template class specifying generic functions for GraphGenerator
 Class
 """
+import os, pickle
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+
+import numpy as np
+import networkx as nx
+import nltk
 
 from utils import *
-import numpy as np
-import os, pickle, nltk
-import networkx as nx
+from GraphGenerator import GraphGenerator
 
-class GraphGenerator:
+class ChainGraph(GraphGenerator):
     def __init__(self, txt_file, emb, word2id, id2word, is_file=True):
         self.emb, self.word2id_dict, self.id2word_dict = emb, word2id, id2word
         self.tokens = self.tokenize(txt_file, is_file)
@@ -17,9 +23,10 @@ class GraphGenerator:
 
     def generate_graph(self):
         tagged = nltk.pos_tag(self.tokens)
-        break_inds = [i for i, word in tagged if word[0] in ['.','!','?']]
+        break_inds = [i for i, word in enumerate(tagged) if word[0] in ['.','!','?']]
         G = nx.DiGraph()
         start_ind = 0
+        print(tagged)
         for i, ind in enumerate(break_inds):
             self.connect_sentence(tagged[start_ind:ind], G, i)
             start_ind = ind + 1
@@ -32,16 +39,16 @@ class GraphGenerator:
 
 
     def connect_sentence(self, sentence, G, sent_num):
-        relationship = set(['VB', 'VBD', 'VBN', 'VBP', 'VBZ', 'IN', 'CC']) #ADP is 'adposition'
+        # relationship = set(['VB', 'VBD', 'VBN', 'VBP', 'VBZ', 'IN', 'CC']) #ADP is 'adposition'
         determiner = set(['WDT', 'DT', 'PDT'])
         names = set(word[0] + "_{}".format(sent_num) for word in sentence \
-                    if word not in relationship and word not in determiner)
+                    if word[1] not in determiner)
         for name in names: G.add_node(name)
-        src_node = sentence[0]
+        src_node = sentence[0][0] + '_{}'.format(sent_num)
         meta_node = "metanode_{}".format(sent_num) # Connects to all other words in sentence
         G.add_node(meta_node)
         for word_i in sentence[1:]:
-            if word_i[1] in relationship or word_i[1] in determiner:
+            if word_i[1] in determiner:
                 continue
             word = word_i[0] + '_{}'.format(sent_num)
             G.add_edge(src_node, word)
@@ -81,3 +88,10 @@ class GraphGenerator:
 
     def id2word(self, id):
         return self.id2word_dict[id]
+
+if __name__=="__main__":
+    emb, word2id_dict, id2word_dict = load_embeddings()
+    gen = ChainGraph("test.txt", emb, word2id_dict, id2word_dict)
+    G = gen.generate_graph()
+    nx.draw_networkx(G)
+    plt.show()
